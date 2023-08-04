26e70a57148fdacf2800bafe579dc282fb52a668
Gyozadaigaku <gaspardcube15@gmail.com>
2023-08-04 13:59:54 +0900

    ♻️ Optimize resource usage by sharing browser instances

--- src/app/api/screenshot/route.ts
+++ src/app/api/screenshot/route.ts
@@ -1,6 +1,9 @@
 import { NextResponse } from 'next/server'
-import puppeteer from 'puppeteer'
+import puppeteer, { Browser } from 'puppeteer'
 const devices = require('puppeteer').devices
+
+// Share one browser instance
+let browserInstance: Browser | null = null
 
 export async function GET(req: Request) {
   const url = new URL(req.url)
@@ -10,69 +13,72 @@ export async function GET(req: Request) {
     return new Response('Invalid href', { status: 400 })
   }
 
-  const urls = await scrapeGoogle(href)
+  try {
+    const urls = await scrapeGoogle(href)
 
-  // take screenshot of the urls
-  let fileNames = []
-  let urlsInfo = []
-  for (let url of urls) {
-    const fileName = await takeScreenshot(url)
-    fileNames.push(fileName)
-    urlsInfo.push({ fileName, url })
+    if (urls.length === 0) {
+      return new Response('Not found', { status: 404 })
+    }
+
+    let fileNames = []
+    let urlsInfo = []
+    for (let url of urls) {
+      const fileName = await takeScreenshot(url)
+      fileNames.push(fileName)
+      urlsInfo.push({ fileName, url })
+    }
+
+    return NextResponse.json({ message: 'success', files: urlsInfo }, { status: 200 })
+  } catch (error) {
+    console.error('Error:', (error as Error).message)
+    return new Response('An error occurred', { status: 500 })
   }
-
-  return NextResponse.json({ message: 'success', files: urlsInfo }, { status: 200 })
 }
 
-async function takeScreenshot(url) {
+async function takeScreenshot(url: string) {
   try {
+    if (!browserInstance) {
+      browserInstance = await puppeteer.launch({ headless: 'new' })
+    }
+
     const iPhone = devices['iPhone X']
-    const browser = await puppeteer.launch({ headless: 'new' })
-    const page = await browser.newPage()
+    const page = await browserInstance.newPage()
     page.setDefaultNavigationTimeout(120000)
     await page.emulate(iPhone)
     await page.goto(url, { waitUntil: 'load', timeout: 120000 })
 
-    // Take a screenshot and get it as a Buffer
     const screenshotBuffer = await page.screenshot({ fullPage: true })
-    await browser.close()
+    await page.close()
 
-    // Convert the Buffer to a base64 string
     const screenshotBase64 = screenshotBuffer.toString('base64')
-
     return screenshotBase64
   } catch (error) {
-    console.error('Error taking screenshot:', error)
+    console.error('Error taking screenshot:', (error as Error).message)
     throw error
   }
 }
 
-async function scrapeGoogle(searchQuery) {
-  const browser = await puppeteer.launch({ headless: 'new' })
-  const page = await browser.newPage()
+async function scrapeGoogle(searchQuery: string) {
+  try {
+    if (!browserInstance) {
+      browserInstance = await puppeteer.launch({ headless: 'new' })
+    }
 
-  const userAgent =
-    'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'
-  const viewport = { width: 375, height: 812 }
-  await page.setUserAgent(userAgent)
-  await page.setViewport(viewport)
+    const page = await browserInstance.newPage()
+    const userAgent =
+      'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'
+    const viewport = { width: 375, height: 812 }
+    await page.setUserAgent(userAgent)
+    await page.setViewport(viewport)
 
-  await page.goto(`https://www.google.com/search?q=${encodeURI(searchQuery)}`)
+    await page.goto(`https://www.google.com/search?q=${encodeURI(searchQuery)}`)
 
-  let searchResults = await page.$$eval('.v5yQqb > a', (links) =>
-    links.map((link) => {
-      const url = link.href
-      // const hashIndex = url.indexOf("#");
-      // if (hashIndex > -1) {
-      //   return url.slice(0, hashIndex);
-      // }
-      return url
-    }),
-  )
+    let searchResults = await page.$$eval('.v5yQqb > a', (links) => links.map((link) => link.href))
 
-  searchResults = searchResults.slice(0, 2) // Limit the number of URLs retrieved
-
-  await browser.close()
-
-  return searchResults
+    searchResults = searchResults.slice(0, 2)
+    return searchResults
+  } catch (error) {
+    console.error('Error scraping Google:', (error as Error).message)
+    throw error
+  }
 }
